39e3a4dc55cd59d780e30721009f02519eb9699b
Mitchel Jansen <mitchel@mexter.nl>
2021-08-03 23:43:37 +0200

    Edited: gebruiker command

--- commands/gebruiker.js
+++ commands/gebruiker.js
@@ -7,7 +7,7 @@
  */
 module.exports = {
     Name: 'gebruiker',
-    async Execute(interaction) {
+    async Execute(interaction, DiscordClient) {
         const InteractionUser = interaction.options.getUser('gebruiker');
 
         // Aan Discord laten weten dat we langer als x seconde nodig hebben om een resultaat terug te geven.
@@ -15,19 +15,31 @@ module.exports = {
         await interaction.defer();
 
         // Tonen welke data er in de InteractionUser zit in de console.
-        console.log(InteractionUser);
+        console.log(await DiscordClient.users.fetch(InteractionUser.id));
 
         // Embed die we tonen in de server met de informatie uit de InteractionUser.
         // Gepersonaliseerd op de gebruiker met zijn of haar data.
         const userEmbed = {
-            title: 'Gebruikersnaam',
-            author: {
-                name: 'Colon',
+            color: 0xe84393,
+            title: 'Informatie over: ' + InteractionUser.username,
+            thumbnail: {
+                url: InteractionUser.displayAvatarURL(),
+            },
+            fields: [
+                {
+                    name: 'ID',
+                    value: InteractionUser.id,
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: 'Colon',
                 icon_url: 'https://i.imgur.com/XCR2lnU.png',
             },
-            description: 'Pascal wilt in de modder spelen met Colon.',
-            content: 'Maar wilt Colon dat wel?',
+            description: 'Pascal wilt graag spruitjes eten met Colon.',
         };
-        interaction.editReply({embed: userEmbed});
+
+        // Verstuur het bericht inclusief de embed.
+        interaction.editReply({content: `Hallo ${interaction.member} 👋, bij deze de opgevraagde informatie!`, embeds: [userEmbed]});
     },
 };
